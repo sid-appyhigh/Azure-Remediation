1963acede54acfa89ec86257c52c3a51b7d24653
aashiqjacob <107754549+aashiqjacob@users.noreply.github.com>
2022-06-20 17:05:07 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -12,11 +12,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = "00000000-0000-0000-0000-000000000000"
-  client_id       = "00000000-0000-0000-0000-000000000000"
-  client_secret   = "000000000000000000000000000000000000"
-  tenant_id       = "00000000-0000-0000-0000-000000000000"
 }
 
 resource "azurerm_resource_group" "testrg" {
